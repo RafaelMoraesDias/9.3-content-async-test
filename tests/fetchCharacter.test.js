66d0bfae95e3be931d887319e497d6a05eec02c9
Rafael Moraes Dias <rafasjbvb@yahoo.com.br>
2022-02-01 16:29:00 -0300

    exercicio 9.3

--- tests/fetchCharacter.test.js
+++ tests/fetchCharacter.test.js
@@ -2,5 +2,13 @@ require('../mocks/fetchSimulator');
 const { fetchCharacter } = require('../src/fetchCharacter');
 
 describe('Teste a função fetchCharacter', () => {
-  it('', () => {});
+  it('testa se o super-heroi é retornado',async () => {
+   const character = await fetchCharacter('720');
+  expect(character.name).toBe('Wonder Woman');
+  });
+
+  it('testa se o parametro for vazio',async () => {
+    const failed = await fetchCharacter();
+   expect(failed).toEqual(new Error('You must provide an url'));
+   });
 });
